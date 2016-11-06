1f20f0aa59f79508bceb4d1e9d4e0462083368a2
Ody Mbegbu <odytrice@gmail.com>
2016-11-06 16:13:34 +0100

    Split Migrations into a Separate Project

--- MigrateDB.bat
+++ MigrateDB.bat
@@ -1,3 +1,1 @@
-rem packages\FluentMigrator.1.6.2\tools\Migrate.exe --db=sqlserver --target=SimpleBlog\bin\SimpleBlog.dll --configPath=SimpleBlog\web.config --c=SimpleBlogDb %*
-
-packages\FluentMigrator.1.6.2\tools\Migrate.exe --db=sqlserver --target=Dumia.Infrastructure\bin\Debug\Dumia.Infrastructure.dll --configPath=Dumia.Http\web.config --c=Dumia %*
+packages\FluentMigrator.1.6.2\tools\Migrate.exe --db=sqlserver --target=Dumia.Migrations\bin\Debug\Dumia.Migrations.dll --configPath=Dumia.Http\web.config --c=Dumia %*
