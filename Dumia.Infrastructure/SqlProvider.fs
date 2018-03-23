486df4f457a744df47f020635eb584e4b02facfe
Ody Mbegbu <odytrice@gmail.com>
2018-03-23 13:37:42 +0100

    Added Support for Repeatable builds

--- Dumia.Infrastructure/SqlProvider.fs
+++ Dumia.Infrastructure/SqlProvider.fs
@@ -6,6 +6,6 @@ open System.Configuration
 [<Literal>]
 let DumiaConn = "Data Source=(local);Initial Catalog=Dumia;Integrated Security=False;User ID=admin1;Password=admin1;MultipleActiveResultSets=True;App=EntityFramework"
 
-type DumiaDB = SqlDataConnection<DumiaConn>
+type DumiaDB = SqlDataConnection<DumiaConn, LocalSchemaFile = "Database.dbml", ForceUpdate = false>
 
 type DBContext = DumiaDB.ServiceTypes.SimpleDataContextTypes.Dumia
--- MigrateDB.bat
+++ MigrateDB.bat
@@ -1,1 +1,6 @@
+REM Migrated Database
 packages\FluentMigrator.1.6.2\tools\Migrate.exe --db=sqlserver --target=Dumia.Migrations\bin\Debug\Dumia.Migrations.dll --configPath=Dumia.Http\web.config --c=Dumia %*
+
+REM Update Database DBML file
+.\utils\SqlMetal.exe /server:"(local)" /database:dumia /user:admin1 /password:admin1 /dbml:"Dumia.Infrastructure/Database.dbml"
+
